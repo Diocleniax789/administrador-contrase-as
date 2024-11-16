707d311aa25a1a508694bcb43e6f9720b86331c8
Diocleniax789 <julioramdom123@gmail.com>
2024-11-16 00:45:31 -0300

    se creo la funcion de carga de usuarios

--- admin-contraseña.c
+++ admin-contraseña.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 struct usuario{
     char cod_usu[8];
@@ -25,4 +28,36 @@ int main(){
     envia_mail(usuarios,cantidad_usuarios_cargados);
 
     return 0;
+}
+
+void carga_usuarios(struct usuario *usuarios,int *cantidad_usuarios_cargados){
+    int i;
+    char cod_usu[8];
+
+    printf("\n - Para finalizar con la carga, al momento de cargar el codigo de usuario, coloque el '*' - \n");
+    printf("\n");
+    printf("\n CARGA DE DATOS DEL USUARIO \n");
+    printf("\n");
+    for(i = 0; i < 10000; i++){
+        system("cls");
+        fflush(stdin);
+        printf("\n Usuario nro %i", i + 1);
+        printf("\n");
+        printf("\n + Codigo de usuario: ");
+        scanf("%s",cod_usu);
+        if(strcmp(usuarios[i].cod_usu,"*") == 0){
+            break;
+        } else{
+            strcpy(usuarios[i].cod_usu,cod_usu);
+            printf("\n + Nombre de usuario: ");
+            scanf("%s",usuarios[i].nom_usu);
+            printf("\n + password: ");
+            scanf("%s",usuarios[i].password);
+            printf("\n + Mail: ");
+            scanf("%s",usuarios[i].mail);
+            (*cantidad_usuarios_cargados)++;
+            printf("\n");
+            system("pause");
+        }
+    }
 }
